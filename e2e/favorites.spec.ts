87edcc932862bf1f31f27a3585d11100889b8264
nimeshjayasankhanj <nimeshappledevelopment@gmail.coml>
2023-07-08 11:55:21 +0530

    feat:change e2e

--- e2e/favorites.spec.ts
+++ e2e/favorites.spec.ts
@@ -8,13 +8,3 @@ test("user should be able to add cocktails to favorite lists", async ({
   await page.getByLabel("search-box-btn").click();
   await page.getByLabel("mark-favorite1").click();
 });
-
-test("user should be able to add cocktails to favorite lists and see it", async ({
-  page,
-}) => {
-  await page.goto(`${process.env.REACT_APP_URL}/home`);
-  await page.getByRole("textbox").type("New");
-  await page.getByLabel("search-box-btn").click();
-  await page.getByLabel("mark-favorite1").click();
-  await page.goto(`${process.env.REACT_APP_URL}/favorites`);
-});
